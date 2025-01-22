72b03ecb87bd8721ec26f9f7724e1ae4e63bdacb
rjp2525 <renophilibert@gmail.com>
2025-01-22 05:33:28 -0700

    Fix format test, warn in the console if same name pan elements are available on the page

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.window.__pan = exports.window.__pan || {
-    csrfToken: "%_PAN_CSRF_TOKEN_%",
-    routePrefix: "%_PAN_ROUTE_PREFIX_%",
-    observer: null,
-    clickListener: null,
-    mouseoverListener: null,
-    inertiaStartListener: null,
-};
+exports.window.__pan =
+    exports.window.__pan ||
+        {
+            csrfToken: "%_PAN_CSRF_TOKEN_%",
+            routePrefix: "%_PAN_ROUTE_PREFIX_%",
+            observer: null,
+            clickListener: null,
+            mouseoverListener: null,
+            inertiaStartListener: null,
+        };
 if (exports.window.__pan.observer) {
     exports.window.__pan.observer.disconnect();
     exports.window.__pan.observer = null;
@@ -17,17 +19,11 @@ if (exports.window.__pan.clickListener) {
     exports.window.__pan.clickListener = null;
 }
 if (exports.window.__pan.mouseoverListener) {
-    document.removeEventListener(
-        "mouseover",
-        exports.window.__pan.mouseoverListener
-    );
+    document.removeEventListener("mouseover", exports.window.__pan.mouseoverListener);
     exports.window.__pan.mouseoverListener = null;
 }
 if (exports.window.__pan.inertiaStartListener) {
-    document.removeEventListener(
-        "inertia:start",
-        exports.window.__pan.inertiaStartListener
-    );
+    document.removeEventListener("inertia:start", exports.window.__pan.inertiaStartListener);
     exports.window.__pan.inertiaStartListener = null;
 }
 (function () {
@@ -45,26 +41,21 @@ if (exports.window.__pan.inertiaStartListener) {
     var impressed = [];
     var hovered = [];
     var clicked = [];
+    var sameNameElements = [];
     var commit = function () {
         if (queue.length === 0) {
             return;
         }
         var onGoingQueue = queue.slice();
         queue = [];
-        navigator.sendBeacon(
-            "/".concat(exports.window.__pan.routePrefix, "/events"),
-            new Blob(
-                [
-                    JSON.stringify({
-                        events: onGoingQueue,
-                        _token: exports.window.__pan.csrfToken,
-                    }),
-                ],
-                {
-                    type: "application/json",
-                }
-            )
-        );
+        navigator.sendBeacon("/".concat(exports.window.__pan.routePrefix, "/events"), new Blob([
+            JSON.stringify({
+                events: onGoingQueue,
+                _token: exports.window.__pan.csrfToken,
+            }),
+        ], {
+            type: "application/json",
+        }));
     };
     var queueCommit = function () {
         queueTimeout && clearTimeout(queueTimeout);
@@ -102,15 +93,18 @@ if (exports.window.__pan.inertiaStartListener) {
     var detectImpressions = function () {
         var elementsBeingImpressed = document.querySelectorAll("[data-pan]");
         elementsBeingImpressed.forEach(function (element) {
-            if (
-                element.checkVisibility !== undefined &&
-                !element.checkVisibility()
-            ) {
+            if (element.checkVisibility !== undefined &&
+                !element.checkVisibility()) {
                 return;
             }
             var name = element.getAttribute("data-pan");
             if (name === null) {
                 return;
+            }
+            var nameElements = document.querySelectorAll("[data-pan='".concat(name, "']"));
+            if (nameElements.length > 1 && !sameNameElements.includes(name)) {
+                console.warn("PAN: Multiple (".concat(nameElements.length, ") elements with the same name '").concat(name, "' found"));
+                sameNameElements.push(name);
             }
             if (impressed.includes(name)) {
                 return;
@@ -125,52 +119,33 @@ if (exports.window.__pan.inertiaStartListener) {
     };
     domObserver(function () {
         impressed.forEach(function (name) {
-            var element = document.querySelector(
-                "[data-pan='".concat(name, "']")
-            );
+            var element = document.querySelector("[data-pan='".concat(name, "']"));
             if (element === null) {
-                impressed = impressed.filter(function (n) {
-                    return n !== name;
-                });
-                hovered = hovered.filter(function (n) {
-                    return n !== name;
-                });
-                clicked = clicked.filter(function (n) {
-                    return n !== name;
-                });
+                impressed = impressed.filter(function (n) { return n !== name; });
+                hovered = hovered.filter(function (n) { return n !== name; });
+                clicked = clicked.filter(function (n) { return n !== name; });
             }
         });
         detectImpressions();
     });
-    exports.window.__pan.clickListener = function (event) {
-        return send(event, "click");
-    };
+    exports.window.__pan.clickListener = function (event) { return send(event, "click"); };
     document.addEventListener("click", exports.window.__pan.clickListener);
     exports.window.__pan.mouseoverListener = function (event) {
         return send(event, "hover");
     };
-    document.addEventListener(
-        "mouseover",
-        exports.window.__pan.mouseoverListener
-    );
+    document.addEventListener("mouseover", exports.window.__pan.mouseoverListener);
     exports.window.__pan.inertiaStartListener = function (event) {
         impressed = [];
         hovered = [];
         clicked = [];
         detectImpressions();
     };
-    document.addEventListener(
-        "inertia:start",
-        exports.window.__pan.inertiaStartListener
-    );
+    document.addEventListener("inertia:start", exports.window.__pan.inertiaStartListener);
     exports.window.__pan.beforeUnloadListener = function (event) {
         if (queue.length === 0) {
             return;
         }
         commit();
     };
-    exports.window.addEventListener(
-        "beforeunload",
-        exports.window.__pan.beforeUnloadListener
-    );
+    exports.window.addEventListener("beforeunload", exports.window.__pan.beforeUnloadListener);
 })();
