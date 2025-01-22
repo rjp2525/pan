4ea093cd3d03d18f74a0e3c488caa0bdeb0a47d1
rjp2525 <renophilibert@gmail.com>
2025-01-22 05:21:45 -0700

    PHPStan + coverage

--- src/Adapters/Laravel/Repositories/DatabaseAnalyticsRepository.php
+++ src/Adapters/Laravel/Repositories/DatabaseAnalyticsRepository.php
@@ -42,12 +42,12 @@ final readonly class DatabaseAnalyticsRepository implements AnalyticsRepository
     {
         /** @var array<int, Analytic> $all */
         $all = DB::table('pan_analytics')->get()->map(fn (mixed $analytic): Analytic => new Analytic(
-            id: (int) $analytic->id,
-            name: $analytic->name,
-            impressions: (int) $analytic->impressions,
-            hovers: (int) $analytic->hovers,
-            clicks: (int) $analytic->clicks,
-            description: $this->config['analytic_descriptions'][$analytic->name] ?? null,
+            id: (int) $analytic->id, // @phpstan-ignore-line
+            name: $analytic->name, // @phpstan-ignore-line
+            impressions: (int) $analytic->impressions, // @phpstan-ignore-line
+            hovers: (int) $analytic->hovers, // @phpstan-ignore-line
+            clicks: (int) $analytic->clicks, // @phpstan-ignore-line,
+            description: $this->config['analytic_descriptions'][$analytic->name] ?? null, // @phpstan-ignore-line
         ))->toArray();
 
         return $all;
